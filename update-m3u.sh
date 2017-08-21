cda3300777284392e7a44435c1d3cf9c2da6108d
dronkeol <cristian.alballay@gmail.com>
2017-08-21 14:53:48 -0300

    nueva version

--- update-m3u.sh
+++ update-m3u.sh
@@ -3,4 +3,5 @@ cd /home/pi/iptv &&
 git pull origin master && 
 wget http://server.poweriptv.cl:8000/get.php\?username\=b1CXurMp\&password\=0n3E1C1sgv\&type\=m3u_plus\&output\=ts --output-document=poweriptv.m3u && 
 git commit poweriptv.m3u -m "Actualizado." && 
-git push 
+git push
+#Finalizado 
