1802a02400a9dd401aa18bea6201f659459f4201
dronkeol <cristian.alballay@gmail.com>
2017-08-21 11:34:36 -0300

    Script actualizador inicial

---
+++ update-m3u.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /home/pi/iptv && 
+git pull origin master && 
+wget http://server.poweriptv.cl:8000/get.php\?username\=b1CXurMp\&password\=0n3E1C1sgv\&type\=m3u_plus\&output\=ts --output-document=poweriptv.m3u && 
+git commit -m "Actualizado." && 
+git push 
